25a70320eeecddfe59491d5004a5d9f30f502170
Diego <dgox16@gmail.com>
2024-03-20 10:09:04 -0600

    Se mejoro el diseño responsivo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		screens: {
+			xs: "380px",
+			...defaultTheme.screens,
+		},
+		extend: {
+			width: {
+				recommend: "115ch",
+			},
+		},
+		// fontFamily: {
+		//     display: ["Onest Variable"],
+		//     body: ["Onest Variable"],
+		// },
 	},
+
 	plugins: [],
-}
+};
